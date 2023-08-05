e711dc9656efa088813e2efcf43ac319f78eaecf
Umor Faruk <140315547+umor2026@users.noreply.github.com>
2023-08-06 03:40:25 +0600

    Add files via upload

---
+++ Assingment_Problem_Solving/Problem 231A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int problemsSolved = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int petya, vasya, tonya;
+        std::cin >> petya >> vasya >> tonya;
+
+        // If the sum of sure views is greater than or equal to 2, increment the count
+        if (petya + vasya + tonya >= 2) {
+            ++problemsSolved;
+        }
+    }
+
+    std::cout << problemsSolved << std::endl;
+
+    return 0;
+}
+
