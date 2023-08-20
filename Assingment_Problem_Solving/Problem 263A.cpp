9e5321e8c7c7d856800a90be3265f8b6a51bcb7c
Umor Faruk <140315547+umor2026@users.noreply.github.com>
2023-08-20 14:50:12 +0600

    Update Problem 263A.cpp

--- Assingment_Problem_Solving/Problem 263A.cpp
+++ Assingment_Problem_Solving/Problem 263A.cpp
@@ -1,37 +1,28 @@
-#include <iostream>
-#include <cmath>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
+int main(){
 
-int main() {
-    int matrix[5][5];
+    int matrix [5][5];
+     int x,y;
+      for (int i=0; i<5;i++){
 
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
 
-    int moves = 0;
+        for(int j=0;j<5;j++){
 
-    int row, col;
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            if (matrix[i][j] == 1) {
-                row = i;
-                col = j;
-                break;
+            cin>> matrix[i][j];
+            if (matrix[i][j]==1){
+
+                x=i;
+                y=j;
+
             }
         }
-    }
+      }
 
-    int target_row = 2;
-    int target_col = 2;
+        int moves=abs(x-2)+abs(y-2);
+        cout<<moves<<endl;
 
-    moves = abs(row - target_row) + abs(col - target_col);
+        return 0;
 
-    cout << moves << endl;
-
-    return 0;
 }
-
