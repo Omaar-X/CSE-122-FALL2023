f7681db20134215d261c7936c235cbed7435de3c
Umor Faruk <140315547+umor2026@users.noreply.github.com>
2023-09-24 15:14:34 +0600

    Create 281A.cpp

---
+++ Assingment_Problem_Solving/281A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+
+    // Capitalize the first letter
+    if (!word.empty()) {
+        word[0] = toupper(word[0]);
+    }
+
+    cout << word << endl;
+
+    return 0;
+}
