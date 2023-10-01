15bfffdcc2b2583ce84b140971ab017aaf3f87c3
Umor Faruk <140315547+umor2026@users.noreply.github.com>
2023-10-01 15:42:55 +0600

    Create Task 266A.cpp

---
+++ Assingment_Problem_Solving/Task 266A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int stones_to_remove = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) {
+            stones_to_remove++;
+        }
+    }
+
+    cout << stones_to_remove << endl;
+
+    return 0;
+}
